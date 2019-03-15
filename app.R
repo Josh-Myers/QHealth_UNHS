36a785c2911420d93ca06db36b7a1779c3f82d45
Josh-Myers <myers.josh@gmail.com>
2019-03-15 10:43:12 +1000

    Remove OR

--- app.R
+++ app.R
@@ -4,18 +4,6 @@ library(shiny)
 model = readRDS("model.rds")
 intercept = -1.564
 
-# descriptions
-Bacterial_meng = "Infants with bacterial meningitis are approximately 3 times more likely to have permanent hearing loss than without this risk factor."
-Cranio = "Infants with craniofacial abnormalities are approximately 7 times more likely to have permanent hearing loss than without this risk factor."
-Family_hx = "Infants with family history of hearing loss are approximately 4 times more likely to have permanent hearing loss than without this risk factor."
-Hyperbili = "Infants with hyperbilirubinaemia are approximately 2 times more likely to have permanent hearing loss than without this risk factor."
-Perinatal_inf = "Infants with perinatal infection are approximately 5 times more likely to have permanent hearing loss than without this risk factor."
-Prof_conc = "Infants with professional concern are approximately 3 times more likely to have permanent hearing loss than without this risk factor."
-Prol_vent = "Infants with prolonged ventilation are approximately 2 times more likely to have permanent hearing loss than without this risk factor."
-Bilat_ref = "Infants who refer in both ears are approximately 4 times more likely to have permanent hearing loss than unilateral."
-Sev_asph = "Infants with severe asphyxia are approximately 4 times more likely to have permanent hearing loss than without this risk factor."
-Syndrome = "Infants with a syndrome are approximately 4 times more likely to have permanent hearing loss than without this risk factor."
-  
 # define UI
 ui <- fluidPage(
    
@@ -49,31 +37,11 @@ ui <- fluidPage(
           ) # close sidebarLayout
      ), # close tabpanel
      
-     tabPanel("Odds Ratios", fluid = TRUE,
-              sidebarLayout(
-                sidebarPanel(
-                  selectInput(inputId = "rf", label = "Select risk factor", 
-                              choices = c("Bacterial meningitis", "Bilateral refer", "Craniofacial abnormalities", "Family history", 
-                                          "Hyperbilirubinaemia", "Perinatal infection", "Professional concern", 
-                                          "Prolonged ventilation", "Severe asphyxia", "Syndrome")),
-                  actionButton('go', "Go"),
-                  br()
-                ), # close sidebarPanel
-                mainPanel(
-                  br(),
-                  textOutput("odds"),
-                  p(),
-                  textOutput("description"),
-                  br()
-                ) # close mainPanel
-              ) # close sidebarLayout
-     ), # close tabpanel
-  
           tabPanel("About", fluid = TRUE,
                   br(),
                   p('This app predicts the likely hearing outcome for a baby who referred on QHealth’s Healthy Hearing Newborn Hearing Screen,  
                     based on subject characteristics and reported risk factors.'),
-                  p('To use the "Risk calculator" tab, select all characteristics that are true. For example, for a female, indigenous baby with family history of hearing loss, 
+                  p('To use the app, select all characteristics that are true. For example, for a female, indigenous baby with family history of hearing loss, 
                     check the "Female gender", "Indigenous", and "Family history" boxes then press the "Go" button'),
                   p('The risk that this child will go on to be diagnosed with permanent hearing loss is then calculated, given the selected risk factors. 
                     Risk is a proportion between 0 and 1,
@@ -87,7 +55,6 @@ ui <- fluidPage(
                     has approximately 40% risk of being diagnosed with permanent hearing loss.'),
                   p('The risk calculator cannot make predictions for infants with: bacterial meningitis, hyperbilirubinaemia, perinatal infection, severe asphyxia, 
                     and professional concern risk factors due to small numbers of babies with these risk factors'),
-                  p('In this case, you can use the "Odds ratios" tab, which calculates the relative odds ratio for a child with a certain risk factor.'),
                   p('For further details, see the pubilcation by Fitzgibbons, J., Driscoll, C., Beswick, R., & Myers, J. xxxxxxxxxxxx (Submitted to xxxxxxxxxxx).')
                   
                   ) # close tabPanel
@@ -111,48 +78,6 @@ server <- function(input, output) {
      pred = predict(model, newdata = data(), type = 'response')
       paste("The risk of hearing loss for this child is", round(pred, 2))
         })
-
-   
-   odds = eventReactive(input$go, {
-   switch(input$rf, 
-          "Bacterial meningitis" = 2.55,
-          "Craniofacial abnormalities" = 7.27,
-          "Family history" = 4.07,
-          "Hyperbilirubinaemia" = 2.23,
-          "Perinatal infection" = 4.77,
-          "Professional concern" = 2.83,
-          "Prolonged ventilation" = 1.82,
-          "Bilateral refer" = 3.8,
-          "Severe asphyxia" = 3.75,
-          "Syndrome" = 4.16
-   )
-   })
-     
-   description = eventReactive(input$go, { 
-     switch(input$rf,
-            "Bacterial meningitis" = Bacterial_meng,
-            "Craniofacial abnormalities" = Cranio,
-            "Family history" = Family_hx,
-            "Hyperbilirubinaemia" = Hyperbili,
-            "Perinatal infection" = Perinatal_inf,
-            "Professional concern" = Prof_conc,
-            "Prolonged ventilation" = Prol_vent,
-            "Bilateral refer" = Bilat_ref,
-            "Severe asphyxia" = Sev_asph,
-            "Syndrome" = Syndrome
-            )
-   })
-   
-   output$odds = renderText({
-     odds = odds()
-     paste("Odds ratio = ", odds)
-   })
-     
-   output$description = renderText({
-     description = description()
-     print(description)
-   })
-   
 }
 
 # Run the application 
